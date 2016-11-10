453674a385c99005ca56398ff192eb96ff0d5ebb
jesusHF <jehernandez1996@gmail.com>
2016-11-10 23:53:54 +0100

    filtrar Mapa

--- WhateverGenNHibernate/CEN/Whatever/MapaCEN_filtrarMapa.cs
+++ WhateverGenNHibernate/CEN/Whatever/MapaCEN_filtrarMapa.cs
@@ -19,13 +19,36 @@ namespace WhateverGenNHibernate.CEN.Whatever
 {
 public partial class MapaCEN
 {
-public void FiltrarMapa (int latitud, int longitud, int radio)
+public void FiltrarMapa (double latitud, double longitud, double radio)
 {
         /*PROTECTED REGION ID(WhateverGenNHibernate.CEN.Whatever_Mapa_filtrarMapa) ENABLED START*/
 
         // Write here your custom code...
 
-        throw new NotImplementedException ("Method FiltrarMapa() not yet implemented.");
+    System.Collections.Generic.IList<MapaEN> listaM= this.GetAll(0,0);
+    System.Collections.Generic.IList<MapaEN> listaMDentro = null;
+
+    foreach (MapaEN map in listaM)
+    {
+        if (map.Evento_mapa2.ID != 0) {
+            double lat = map.Latitud;
+            double lon = map.Longitud;
+
+            var difLatitud  = Convert.ToSingle(Math.Abs(latitud - lat) * (Math.PI / 180));
+            var difLongitud = Convert.ToSingle(Math.Abs(longitud - lon) * (Math.PI / 180));
+
+
+
+            var a = Math.Pow(Math.Sin(difLatitud / 2), 2) + (Math.Cos(latitud) * Math.Cos(lat) * Math.Pow(Math.Sin(difLongitud / 2), 2));
+            a = Convert.ToDouble(a);
+            double sqr1 = Math.Sqrt(a);
+            double sqr2 = Math.Sqrt(1-a);
+
+            double c = 2 * Math.Atan2 (sqr1, sqr2);
+            if (c * 6370000 > radio)
+                listaMDentro.Add(map);
+        }
+    }
 
         /*PROTECTED REGION END*/
 }
