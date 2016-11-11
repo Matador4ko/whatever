ed42c977cbdfc901c5eb82790dc53eb0e24fd333
jesusHF <jehernandez1996@gmail.com>
2016-11-11 22:54:26 +0100

    Filtrar mapa
    el weno

--- Codigo/WhateverGenNHibernate/CEN/Whatever/MapaCEN_FiltrarMapa.cs
+++ Codigo/WhateverGenNHibernate/CEN/Whatever/MapaCEN_FiltrarMapa.cs
@@ -19,38 +19,38 @@ namespace WhateverGenNHibernate.CEN.Whatever
 {
 public partial class MapaCEN
 {
-public System.Collections.Generic.IList<WhateverGenNHibernate.EN.Whatever.MapaEN> FiltrarMapa (int latitud, int longitud, int radio)
-{
+    public void FiltrarMapa(int latitud, int longitud, int radio)
+    {
         /*PROTECTED REGION ID(WhateverGenNHibernate.CEN.Whatever_Mapa_filtrarMapa) ENABLED START*/
 
         // Write here your custom code...
 
 
-        System.Collections.Generic.IList<MapaEN> listaM = FiltrarEventos ();
+        System.Collections.Generic.IList<MapaEN> listaM = this.GetAll(0, 0);
         System.Collections.Generic.IList<MapaEN> listaMDentro = null;
 
-        foreach (MapaEN map in listaM) {
+        foreach (MapaEN map in listaM)
+        {
+            if (map.Evento_mapa2.ID != 0)
+            {
                 double lat = map.Latitud;
                 double lon = map.Longitud;
 
-                var difLatitud = Convert.ToSingle (Math.Abs (latitud - lat) * (Math.PI / 180));
-                var difLongitud = Convert.ToSingle (Math.Abs (longitud - lon) * (Math.PI / 180));
+                double EarthRadius = 6371;
+
+                double distance = 0;
+                double Lat = (lat - latitud) * (Math.PI / 180);
+                double Lon = (lon - longitud) * (Math.PI / 180);
+                double a = Math.Sin(Lat / 2) * Math.Sin(Lat / 2) + Math.Cos(latitud * (Math.PI / 180)) * Math.Cos(lat * (Math.PI / 180)) * Math.Sin(Lon / 2) * Math.Sin(Lon / 2);
+                double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+                distance = EarthRadius * c;
 
 
-
-                var a = Math.Pow (Math.Sin (difLatitud / 2), 2) + (Math.Cos (latitud) * Math.Cos (lat) * Math.Pow (Math.Sin (difLongitud / 2), 2));
-                a = Convert.ToDouble (a);
-                double sqr1 = Math.Sqrt (a);
-                double sqr2 = Math.Sqrt (1 - a);
-
-                double c = 2 * Math.Atan2 (sqr1, sqr2);
-                if (c * 6370000 > radio) {
-                        listaMDentro.Add (map);
-                }
+                if (distance < radio)
+                    listaMDentro.Add(map);  //HAY QUE DEVOLVER COMO????????????
+            }
         }
-        return listaMDentro;
-
         /*PROTECTED REGION END*/
-}
+    }
 }
 }
